0a68e37fd2ceff79e87a1f6bb7f8224b2d10b9dc
ohyonghao <github@ohyonghao.com>
2016-09-04 12:19:36 -0700

    Tutorial 4 - Networking
    Here we've added networking with PUN and setup the spawning of our
    characters at random points in the world which was an idea added by me
    here.  Certainly quill18 will address this in a later tutorial.

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    [SerializeField]
+    bool OfflineMode = false;
+    public Camera standbyCamera;
+
+	// Use this for initialization
+	void Start () {
+        Connect();
+	}
+	
+	// Update is called once per frame
+	void Connect () {
+        PhotonNetwork.offlineMode = OfflineMode;
+        PhotonNetwork.ConnectUsingSettings("MultiFPS 0.0.1");
+	}
+
+    void OnGUI() {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    void OnJoinedLobby() {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed() {
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom() {
+        Debug.Log("Room Joined");
+
+        SpawnMyPlayer();
+    }
+
+    void SpawnMyPlayer() {
+        PhotonNetwork.Instantiate("FPSController", RandomSpawnVector3(), Quaternion.identity, 0);
+        standbyCamera.enabled = false;
+    }
+
+    Vector3 RandomSpawnVector3() {
+        GameObject[] respawn = GameObject.FindGameObjectsWithTag("Respawn");
+        return respawn[Random.Range(0,respawn.Length)].transform.position;
+    }
+}
